a5fe7278952cdf2f70663ebf3caa05884eb31277
Marcos Paulo De Almeida Lima <marcospaulo.mpa@gmail.com>
2023-01-06 09:20:11 -0300

    Tarefa-787878 - alterando arquivo de configuração git
    - modified:   gitconfig.sh

--- gitconfig.sh
+++ gitconfig.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 rm -f .git/hooks/prepare-commit-msg
-ln -s prepare-commit-msg .git/hooks/prepare-commit-msg
+ln -s ../../prepare-commit-msg ./.git/hooks/prepare-commit-msg
+ls -al ./.git/hooks
